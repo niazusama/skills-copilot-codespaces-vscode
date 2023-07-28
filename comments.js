d84045f5120d19084b9998277f7d03451849a0cc
niazusama <131873257+niazusama@users.noreply.github.com>
2023-07-28 16:02:52 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,33 @@
 // create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Use body-parser middleware to parse JSON data
+app.use(bodyParser.json());
+
+// Store comments in an array (for simplicity, not for production use)
+const comments = [];
+
+// Define a route to handle new comments
+app.post('/comments', (req, res) => {
+  const { author, comment } = req.body;
+  if (!author || !comment) {
+    return res.status(400).json({ error: 'Author and comment are required' });
+  }
+
+  const newComment = { author, comment };
+  comments.push(newComment);
+  res.status(201).json(newComment);
+});
+
+// Define a route to get all comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// Start the server on port 3000
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
